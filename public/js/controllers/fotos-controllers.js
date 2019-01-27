a9a8366e8830fe3b34dca29f3a5256f4be1f2847
Marcus Menezes <mvgmenezes@gmail.com>
2019-01-27 11:14:29 -0200

    Usando  e promise para consulta da api

--- public/js/controllers/fotos-controllers.js
+++ public/js/controllers/fotos-controllers.js
@@ -2,24 +2,24 @@
 
 //criando um controller dentro do modulo alurapic, basta chamar o .controller() passando o nome do 
 //controller 'FotosController' e o segundo parametro é a funcao que define o controller
-angular.module('alurapic').controller('FotosController', function ($scope) { 
-    //a varialvel foto (var foto) é privada e para que a view(html) consiga visualiza-la nao é um bom padrao 
+angular.module('alurapic').controller('FotosController', function ($scope, $http) { 
+    //$scope - a varialvel foto (var foto) é privada e para que a view(html) consiga visualiza-la nao é um bom padrao 
     //transforma-la em publica, pois somente a variavel 'angular' é publica, para isso usamos o  $scope
     //portanto deve-se alterar de var foto para $scope.foto
 
-    //definindo as propriedades do item foto, onde o index html acessa
-    $scope.fotos = [
-    {
-        titulo: 'Leão',
-        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    },  
-    {
-        titulo: 'Leão 2',
-        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    } , 
-    {
-        titulo: 'Leão 3',
-        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    }];
+    //$http - responsavel em fazer chamadas http (consumo de API, etc...) - Assincrona
+
+    $scope.fotos = [];
+
+    //pelo o $http ser assincrono ele nao garante que executara corretamente, por isso ele te dara
+    //uma promessa que irá executar. promisse
+    var promise = $http.get('v1/fotos');
+
+    //se o promise conseguir pegar os dados entao (than)
+    promise.then(function(retorno){
+        $scope.fotos = retorno.data;
+    }).catch(function(error){
+        console.log(error);
+    });
 
 });
