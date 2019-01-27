fa1789a0ca4ef122952910555a728134ef1f0707
Marcus Menezes <mvgmenezes@gmail.com>
2019-01-27 10:13:03 -0200

    Alterando a variavel foto para scope

--- public/js/controllers/fotos-controllers.js
+++ public/js/controllers/fotos-controllers.js
@@ -2,10 +2,13 @@
 
 //criando um controller dentro do modulo alurapic, basta chamar o .controller() passando o nome do 
 //controller 'FotosController' e o segundo parametro é a funcao que define o controller
-angular.module('alurapic').controller('FotosController', function () {
+angular.module('alurapic').controller('FotosController', function ($scope) { 
+    //a varialvel foto (var foto) é privada e para que a view(html) consiga visualiza-la nao é um bom padrao 
+    //transforma-la em publica, pois somente a variavel 'angular' é publica, para isso usamos o  $scope
+    //portanto deve-se alterar de var foto para $scope.foto
 
     //definindo as propriedades do item foto, onde o index html acessa
-    var foto = {
+    $scope.foto = {
         titulo: 'Leão',
         url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
     };
