64a4e052987fbc0730f1191ebca2285be9b40fa9
Marcus Menezes <mvgmenezes@gmail.com>
2019-01-27 12:14:01 -0200

    Alternado a directive para um arquivo html ddo.templateUrl

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,54 @@
+//criando um modulo chamado minhas diretivas onde terá o objeto html de imagem para modulariar o html
+//o angular troca o camel case (meuPainel) para <meu-painel> no momento que for usar na view. 
+angular.module('minhasDiretivas', []).directive('meuPainel', function(){
+
+    //quando se cria uma diretiva deve-se sempre retornar um ddo (directive definition object)
+    var ddo = {};
+
+    //dizendo qual a restricao de uso do ddo
+    //A = atributo , E elemento
+    ddo.restric = "AE";
+
+    //criando um escopo privado para poder ser reutilizados em varias views sem baguncar o scope 
+    ddo.scope = {
+        //define qual dado será privado na diretiva
+        /* 
+            ddo.scope = {
+                titulo: '@tituloPrivado'
+            };
+
+            <meu-painel tituloPrivado = "Leao">
+            </meu-painel>
+            
+            Quando a diretiva é igual o atributo definido = titulo: '@titulo' pode-se colcoar diretamente 
+            somente o @ ficando titulo:'@'
+
+            <meu-painel titulo = "Leao">
+            </meu-painel>
+        */
+        titulo: '@'
+    };
+
+    //definindo para minha diretiva mantenha os elementos filhos (declarados dentro da view, neste caso como <img> 
+    //no momento que for processado o html 
+    // e dentro do meu ddo.template indico qual deve ser mantido os filhos (ng-transclude)
+    ddo.transclude = true;
+
+    //defindo o template da diretiva
+    /*
+    ddo.template = 
+                '<div class="panel panel-default">'
+            +   '   <div class="panel-heading">'
+            +   '        <h3 class="panel-title text-center">{{titulo}}</h3> ' //{{titulo}} está sendo recuperado do escopo privado
+            +   '   </div>'
+            +   '   <div class="panel-body" ng-transclude>'
+            +   '   </div>'
+            +   '</div>'
+    */
+
+    //substituindo o ddo.template por um html
+    ddo.templateUrl = 'js/directives/meu-painel.html';
+
+    return ddo;
+
+})
