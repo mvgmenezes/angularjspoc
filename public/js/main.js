1304413cd3b48bd795eb797f46f56f20d540adbb
Marcus Vinicius Gomes de Menezes <marcus.menezes@elumini.com.br>
2019-01-28 19:01:54 -0200

    Iniciando uma ng-view e configurando uma rota no .config do main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
 //#####Primeira etapa criar o modulo para trabalhar com a foto.#####
 
 //criando um modulo, incluindo a diretiva criada minhasDiretivas
-angular.module('alurapic', ['minhasDiretivas']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute']);
