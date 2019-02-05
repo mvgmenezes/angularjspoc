820ca3893734c544084ff984ada34c9b48560b0d
Marcus Menezes <mvgmenezes@gmail.com>
2019-02-05 15:06:28 -0200

    Validando o form e salvando uma nova foto

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,8 +1,20 @@
 //criando o Controller FotoController
-angular.module('alurapic').controller('FotoController', function($scope){
+angular.module('alurapic').controller('FotoController', function($scope, $http){
     $scope.foto = {};
 
     $scope.submeter = function() {
        console.log($scope.foto);
+
+        //verificando se os dados enviadaos foram validos
+        if ($scope.formulario.$valid) {
+            //enviando os dados para o salvar.
+            $http.post('/v1/fotos', $scope.foto)
+            .success(function() {
+                console.log('Foto adicionada com sucesso');
+            })
+            .error(function(erro) {
+                console.log('Não foi possível cadastrar a foto');
+            })
+        }
     }
 })
--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,54 +1,72 @@
 //criando um modulo chamado minhas diretivas onde terá o objeto html de imagem para modulariar o html
 //o angular troca o camel case (meuPainel) para <meu-painel> no momento que for usar na view. 
-angular.module('minhasDiretivas', []).directive('meuPainel', function(){
+angular.module('minhasDiretivas', [])
 
-    //quando se cria uma diretiva deve-se sempre retornar um ddo (directive definition object)
-    var ddo = {};
+    .directive('meuPainel', function(){
 
-    //dizendo qual a restricao de uso do ddo
-    //A = atributo , E elemento
-    ddo.restric = "AE";
+        //quando se cria uma diretiva deve-se sempre retornar um ddo (directive definition object)
+        var ddo = {};
 
-    //criando um escopo privado para poder ser reutilizados em varias views sem baguncar o scope 
-    ddo.scope = {
-        //define qual dado será privado na diretiva
-        /* 
-            ddo.scope = {
-                titulo: '@tituloPrivado'
-            };
+        //dizendo qual a restricao de uso do ddo
+        //A = atributo , E elemento
+        ddo.restric = "AE";
 
-            <meu-painel tituloPrivado = "Leao">
-            </meu-painel>
-            
-            Quando a diretiva é igual o atributo definido = titulo: '@titulo' pode-se colcoar diretamente 
-            somente o @ ficando titulo:'@'
+        //criando um escopo privado para poder ser reutilizados em varias views sem baguncar o scope 
+        ddo.scope = {
+            //define qual dado será privado na diretiva
+            /* 
+                ddo.scope = {
+                    titulo: '@tituloPrivado'
+                };
 
-            <meu-painel titulo = "Leao">
-            </meu-painel>
+                <meu-painel tituloPrivado = "Leao">
+                </meu-painel>
+                
+                Quando a diretiva é igual o atributo definido = titulo: '@titulo' pode-se colcoar diretamente 
+                somente o @ ficando titulo:'@'
+
+                <meu-painel titulo = "Leao">
+                </meu-painel>
+            */
+            titulo: '@'
+        };
+
+        //definindo para minha diretiva mantenha os elementos filhos (declarados dentro da view, neste caso como <img> 
+        //no momento que for processado o html 
+        // e dentro do meu ddo.template indico qual deve ser mantido os filhos (ng-transclude)
+        ddo.transclude = true;
+
+        //defindo o template da diretiva
+        /*
+        ddo.template = 
+                    '<div class="panel panel-default">'
+                +   '   <div class="panel-heading">'
+                +   '        <h3 class="panel-title text-center">{{titulo}}</h3> ' //{{titulo}} está sendo recuperado do escopo privado
+                +   '   </div>'
+                +   '   <div class="panel-body" ng-transclude>'
+                +   '   </div>'
+                +   '</div>'
         */
-        titulo: '@'
-    };
 
-    //definindo para minha diretiva mantenha os elementos filhos (declarados dentro da view, neste caso como <img> 
-    //no momento que for processado o html 
-    // e dentro do meu ddo.template indico qual deve ser mantido os filhos (ng-transclude)
-    ddo.transclude = true;
+        //substituindo o ddo.template por um html
+        ddo.templateUrl = 'js/directives/meu-painel.html';
 
-    //defindo o template da diretiva
-    /*
-    ddo.template = 
-                '<div class="panel panel-default">'
-            +   '   <div class="panel-heading">'
-            +   '        <h3 class="panel-title text-center">{{titulo}}</h3> ' //{{titulo}} está sendo recuperado do escopo privado
-            +   '   </div>'
-            +   '   <div class="panel-body" ng-transclude>'
-            +   '   </div>'
-            +   '</div>'
-    */
+        return ddo;
 
-    //substituindo o ddo.template por um html
-    ddo.templateUrl = 'js/directives/meu-painel.html';
+    })
 
-    return ddo;
+    .directive('minhaFoto', function() {
 
-})
+        var ddo = {};
+
+        ddo.restrict = "AE";
+
+        ddo.scope = {
+            titulo: '@',
+            url: '@'
+        };
+
+        ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
+        
+        return ddo;
+    });
