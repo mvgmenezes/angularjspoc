9da40b93693e392e45e7573cf776efc584eb1fc6
Marcus Menezes <mvgmenezes@gmail.com>
2019-02-05 15:15:14 -0200

    Apresetando a mensagem e limpando o form

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,6 +1,7 @@
 //criando o Controller FotoController
 angular.module('alurapic').controller('FotoController', function($scope, $http){
     $scope.foto = {};
+    $scope.mensagem = '';
 
     $scope.submeter = function() {
        console.log($scope.foto);
@@ -10,9 +11,13 @@ angular.module('alurapic').controller('FotoController', function($scope, $http){
             //enviando os dados para o salvar.
             $http.post('/v1/fotos', $scope.foto)
             .success(function() {
+                //limpoando o objeto foto 
+                $scope.foto = {};
+                $scope.mensagem = 'Foto adicionada com sucesso';
                 console.log('Foto adicionada com sucesso');
             })
             .error(function(erro) {
+                $scope.mensagem = 'Não foi possível cadastrar a foto';
                 console.log('Não foi possível cadastrar a foto');
             })
         }
